b7439e6b0940bb5cbdaa1228aef176b990925963
Gabriela Cordova <gabrielacordovaa@hotmail.com>
2022-01-13 19:48:27 -0300

    project initialized

---
+++ src/main/java/br/com/rest/webservices/springbootapimodel/SpringBootApiModelApplication.java
@@ -0,0 +1,13 @@
+package br.com.rest.webservices.springbootapimodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApiModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApiModelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rest/webservices/springbootapimodel/SpringBootApiModelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rest.webservices.springbootapimodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApiModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
